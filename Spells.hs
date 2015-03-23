69e05b3979e4a66cfe60f32c25217067b3e3ccc2
M. Monster <horatius83@gmail.com>
2015-03-22 20:39:31 -0500

    Updated Spells
    Combat Spells are finished

--- Spells.hs
+++ Spells.hs
@@ -11,6 +11,7 @@ import Data.Map (Map, fromList, findWithDefault)
 
 data SpellType = Physical | Mana deriving (Show)
 data SpellRange = LoS | LoSArea | Touch | Voluntary | Area  deriving (Show)
+data SpellDamage = Sd_Physical | Sd_Stun deriving (Show)
 data SpellDuration = Instant | Sustained | Permanent deriving (Show)
 data DV = DV String deriving (Show)
 data CombatSpellType = Cst_Direct | Cst_Indirect deriving (Show)
@@ -22,7 +23,7 @@ data IllusionSpellSenses = Ist_Single | Ist_Multi deriving (Show)
 data ManipulationSpellType = Mst_Mental | Mst_Physical | Mst_Environmental deriving (Show)
 
 data Spell = 
-        CombatSpell String SpellType SpellRange SpellDuration DV String CombatSpellType ElementalType |
+        CombatSpell String SpellType SpellRange SpellDamage SpellDuration DV String CombatSpellType ElementalType |
         DetectionSpell String SpellType SpellRange SpellDuration DV String DetectionSpellType DetectionSpellFocus |
         Spell String SpellType SpellRange SpellDuration DV String |
         IllusionSpell String SpellType SpellRange SpellDuration DV String IllusionSpellType IllusionSpellSenses |
@@ -30,7 +31,7 @@ data Spell =
 
 
 spellName :: Spell -> String    
-spellName (CombatSpell name _ _ _ _ _ _ _ ) = name
+spellName (CombatSpell name _ _ _ _ _ _ _ _ ) = name
 spellName (DetectionSpell name _ _ _ _ _ _ _) = name
 spellName (Spell name _ _ _ _ _) = name
 spellName (IllusionSpell name _ _ _ _ _ _ _) = name
@@ -44,11 +45,21 @@ getSpell x = findWithDefault (notFoundSpell x) x spellDb
 
 spellDb :: Map String Spell
 spellDb = fromList [(spellName x, x) | x <- [
-{-    Spell "Armor" Physical LoS Sustained "Both ballistic and Impact protection equal to hits scored, cumulative with worn armor_",
-    Spell "Confusion" Mana LoS Sustained "-1 dice pool modifier to target per hit",
-    Spell "Ice Sheet" Physical Area Instant "Crossing ice requires Agility + Reaction Test, Threshold equal to hits, to avoid falling",
-    Spell "Manabolt" Mana LoS Instant "Damage: (equal to hits)P"],
-]-}
-    CombatSpell "Acid Stream" Physical LoS Instant (DV "F/2+3") "" Cst_Indirect Acid,
-    CombatSpell "Toxic Wave" Physical LoSArea Instant (DV "F/2+5") "" Cst_Indirect Acid,
-    CombatSpell "Punch" Physical Touch Instant (DV "F/2-2") "" Cst_Indirect None]]
+    CombatSpell "Acid Stream" Physical LoS Sd_Physical Instant (DV "F/2+3") "" Cst_Indirect Acid,
+    CombatSpell "Toxic Wave" Physical LoSArea Sd_Physical Instant (DV "F/2+5") "" Cst_Indirect Acid,
+    CombatSpell "Punch" Physical Touch Sd_Stun Instant (DV "F/2-2") "" Cst_Indirect None,
+    CombatSpell "Clout" Physical LoS Sd_Stun Instant (DV "F/2") "" Cst_Indirect None,
+    CombatSpell "Blast" Physical LoSArea Sd_Stun Instant (DV "F/2+2") "" Cst_Indirect None,
+    CombatSpell "Death Touch" Mana Touch Sd_Physical Instant (DV "F/2-2") "" Cst_Direct None,
+    CombatSpell "Manabolt" Mana LoSArea Sd_Physical Instant (DV "F/2") "" Cst_Direct None,
+    CombatSpell "Manaball" Mana LoSArea Sd_Physical Instant (DV "F/2+2") "" Cst_Direct None,
+    CombatSpell "Flamethrower" Physical LoS Sd_Physical Instant (DV "F/2+3") "" Cst_Indirect Fire,
+    CombatSpell "Fireball" Physical LoSArea Sd_Physical Instant (DV "F/2+5") "" Cst_Indirect Fire,
+    CombatSpell "Lightning Bolt" Physical LoS Sd_Physical Instant (DV "F/2+3") "" Cst_Indirect Lightning,
+    CombatSpell "Ball Lightning" Physical LoSArea Sd_Physical Instant (DV "F/2+5") "" Cst_Indirect Lightning,
+    CombatSpell "Shatter" Physical Touch Sd_Physical Instant (DV "F/2-1") "" Cst_Direct None,
+    CombatSpell "Powerbolt" Physical LoS Sd_Physical Instant (DV "F/2+1") "" Cst_Direct None,
+    CombatSpell "Powerball" Physical LoSArea Sd_Physical Instant (DV "F/2+3") "" Cst_Direct None,
+    CombatSpell "Knockout" Mana Touch Sd_Stun Instant (DV "F/2-3") "" Cst_Direct None,
+    CombatSpell "Stunbolt" Mana LoS Sd_Stun Instant (DV "F/2-1") "" Cst_Direct None,
+    CombatSpell "Stunball" Mana LoSArea Sd_Stun Instant (DV "F/2+1") "" Cst_Direct None]]
