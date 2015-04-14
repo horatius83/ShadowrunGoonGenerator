9394ab2542b7d74df8deb1a6b6623718c85bb882
M Monster <horatius83@gmail.com>
2015-04-14 18:58:58 -0500

    More Changes to Cyberware

--- Cyberware.hs
+++ Cyberware.hs
@@ -1,7 +1,5 @@
 module Cyberware (Cyberware(..), BodyPart(..), CyberLimbEnhancement(..)) where
 
-import Stats (Stats)
-
 data BodyPart = Arm | Leg | Torso | Hand | Foot | Forearm | LowerLeg | Head | Eyes | Ears deriving (Show)
 data CyberLimbEnhancement = Armor Int | Body Int | Strength Int | Agility Int deriving (Show)
 data Cyberware = CyberLimb BodyPart CyberLimbEnhancement Double Int | Cyberware BodyPart Double Int deriving (Show)
@@ -21,9 +19,9 @@ headware = [
     ("Tooth Breakable Compartment", 0.0, 0),
     ("Ultrasound Sensor", 0.3, 2),
     ("Voice Modulator", 0.2, 0)] 
-        ++ [("Olfactory Booster " ++ (show i), 0.2, 2) | i <- [1..6]] 
-        ++ [("Taste Booster " ++ (show i), 0.2, 0) |  i <- [1..6]]
-        ++ [("Voice Modulator w/ Secondary Pattern " ++ (show i), 0.2, 0) | i <- [1..6]]
+        ++ [("Olfactory Booster " ++ (show i), 0.2, 2) | i <- [1..6] :: [Int]] 
+        ++ [("Taste Booster " ++ (show i), 0.2, 0) |  i <- [1..6] :: [Int]]
+        ++ [("Voice Modulator w/ Secondary Pattern " ++ (show i), 0.2, 0) | i <- [1..6] :: [Int]]
 
 eyeware :: [(String, Double, Int)]
 eyeware = [
@@ -36,9 +34,9 @@ eyeware = [
     ("Smartlink", 0.1, 3),
     ("Thermographic Vision", 0.1, 2),
     ("Vision Magnification", 0.1, 2)]
-        ++ [("Cybereyes " ++ (show i), 0.1 + (fromIntegral i) * 0.1, i * 4) | i <- [1..4]] 
-        ++ [("Retinal Duplication " ++ (show i), 0.1, 1) | i <- [1..6]]
-        ++ [("Vision Enhancement " ++ (show i), 0.1, i) | i <- [1..3]]
+        ++ [("Cybereyes " ++ (show i), 0.1 + (fromIntegral i) * 0.1, i * 4) | i <- [1..4] :: [Int]] 
+        ++ [("Retinal Duplication " ++ (show i), 0.1, 1) | i <- [1..6] :: [Int]]
+        ++ [("Vision Enhancement " ++ (show i), 0.1, i) | i <- [1..3] :: [Int]]
 
 earware :: [(String, Double, Int)]
 earware = [
@@ -47,9 +45,43 @@ earware = [
     ("Ear Recording Unit", 0.1, 0),
     ("Sound Link", 0.1, 0),
     ("Spatial Recognizer", 0.1, 2)]
-        ++ [("Cyberears " ++ (show i), 0.1 + (fromIntegral i) * 0.1, 4 * i) | i <- [1..4]]
-        ++ [("Audio Enhancement " ++ (show i), 0.1, i) | i <- [1..3]]
-        ++ [("Select Sound Filter " ++ (show i), 0.1, i) | i <- [1..6]]
+        ++ [("Cyberears " ++ (show i), 0.1 + (fromIntegral i) * 0.1, 4 * i) | i <- [1..4] :: [Int]]
+        ++ [("Audio Enhancement " ++ (show i), 0.1, i) | i <- [1..3] :: [Int]]
+        ++ [("Select Sound Filter " ++ (show i), 0.1, i) | i <- [1..6] :: [Int]]
 
 bodyware :: [(String, Double, Int)]
-bodyware = undefined
+bodyware = [ 
+    ("Plastic Bone Lacing", 0.5, 0),
+    ("Aluminum Bone Lacing", 1.0, 0),
+    ("Titanium Bone Lacing", 1.5, 0),
+    ("Cosmetic Modification", 0.0, 0),
+    ("Fingertip Compartment", 0.1, 1),
+    ("Grapple Gun", 0.5, 5),
+    ("Internal Air Tank", 0.25, 3),
+    ("Simrig", 0.5, 0),
+    ("Smuggling Compartment", 0.2, 2),
+    ("Touch Link", 0.1, 0)]
+        ++ [("Dermal Plating " ++ (show i), 0.5 * (fromIntegral i), 0) | i <- [1..3] :: [Int]]
+        ++ [("Muscle Replacement " ++ (show i), fromIntegral i, 0) | i <- [1..4] :: [Int]]
+        ++ [("Reaction Enhancers " ++ (show i), 0.3 * fromIntegral i, 0) | i <- [1..3] :: [Int]]
+        ++ [("Skillwires " ++ (show i), 0.2 * fromIntegral i, 0) | i <- [1..5] :: [Int]]
+        ++ [("Wired Reflexes " ++ (show i), j, 0) | (i,j) <- [(1, 2.0), (2, 3.0), (3, 5.0)] :: [(Int, Double)]] 
+
+cyberlimbs :: [(String, Double, Int)]
+cyberlimbs = [("Obvious " ++ (show i), j, k) | (i,j,k) <- [
+    ("Full Arm", 1.0, 15),
+    ("Full Leg", 1.0, 20),
+    ("Hand", 0.25, 4),
+    ("Foot", 0.25, 4),
+    ("Lower Arm", 0.45, 10),
+    ("Lower Leg", 0.45, 12),
+    ("Torso", 1.5, 10),
+    ("Skull", 0.75, 4)]]
+        ++ [("Synthetic " ++ (show i), j, k) | (i,j,k) <- [
+                ("Full Arm", 1.0, 8),
+                ("Full Leg", 1.0, 10),
+                ("Hand", 0.25, 2),
+                ("Foot", 0.25, 2),
+                ("Lower Arm", 0.45, 5),
+                ("Lower Leg", 0.45, 6),
+                ("Torso", 1.5, 5)]]
